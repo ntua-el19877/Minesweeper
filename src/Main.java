2370fb861721f3bbf1ad69b464bd1237e156df84
ntua-el19877 <el19877@mail.ntua.gr>
2022-11-19 17:10:53 +0200

    readFile rads only int and has exceptions

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+/*
+ * Angelos Loukas
+ * Minesweeper Project 
+ * Texnologi polumeson
+ * 18/11/22
+ */
+
+public class Main {
+
+ 
+   public static void main(String []args) throws Exception {
+       System.out.println("Hello Worlddddd"); // prints Hello World
+       int arr[]=new int[4];
+       arr=ReadFile.main("SCENARIO-2");
+       System.out.println(arr[1]);
+    }
+ }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -5,38 +5,80 @@ package src;
  
 // Importing input output classes
 import java.io.*;
+import java.util.Scanner;
  
 // Main class
 public class ReadFile {
  
-    // main driver method
-    public static int[] main(String string) throws Exception
+    /*returns 4 item array of text file */
+    private static int[] read_scenario(String string)
     {
- 
+        int scenario_numbers[]=new int[4];
+
         // File path is passed as parameter
-        File file = new File(
-            "/home/angelos/Documents/JavaFiles/Minesweeper/src/"+string);
+        String scenario_id_file = new String("/home/angelos/Documents/JavaFiles/Minesweeper/src/"+string+".txt");
  
         // Note:  Double backquote is to avoid compiler
         // interpret words
         // like \test as \t (ie. as a escape sequence)
  
         // Creating an object of BufferedReader class
-        BufferedReader br
-            = new BufferedReader(new FileReader(file));
-        
-        int scenario_numbers[]=new int[4];
-        // Declaring a string variable
-        String st;
-        // Condition holds true till
-        // there is character in a string
-
-        for(int i=0;i<4;i++)
+        try(Scanner scanner = new Scanner(new File(scenario_id_file))) {
+            
+            for (int i =0;i<4;i++)
+            {
+                scenario_numbers[i]=scanner.nextInt();
+            }
+           
+        }
+        catch(Exception BufferedReader1)
         {
-            st = br.readLine();
-            scenario_numbers[i] = Integer.parseInt(st);
-            System.out.println(st);
+            System.out.println("Erros occured while reading "+string+".txt");
         }
         return scenario_numbers;
+        
     }
+    
+    private static int[] read_scenario_margins(String string)
+    {
+        int scenario_numbers_margins[]=new int[8];
+        //read SCENARIO-ID-DIFFICULTY-MARGINS to throw exceptions if needed
+        String scenario_id_dif_margins_file=new String("/home/angelos/Documents/JavaFiles/Minesweeper/src/"+string+"-DIFFICULTY-MARGINS.txt");
+        
+        try(Scanner scanner = new Scanner(new File(scenario_id_dif_margins_file))) {
+            /*
+                 * num1:diff. level
+                 * num2&num3: mesh size
+                 * num4&num5: bombs
+                 * num6&num7: time
+                 * num8: mega bomb
+                 */
+            for (int i =0;i<8;i++)
+            {
+                scenario_numbers_margins[i]=scanner.nextInt();
+            }
+           
+        }
+        catch(Exception BufferedReader2)
+        {
+            System.out.println("Erros occured while reading "+string+"-DIFFICULTY-MARGINS.txt");
+        }
+        return scenario_numbers_margins;
+    }
+    
+    // main driver method
+    public static int[] main(String string) throws Exception
+    {
+          
+        int scenario_numbers[]=new int[4];
+        int scenario_numbers_margins[]=new int[8];
+
+        scenario_numbers=read_scenario(string);
+        scenario_numbers_margins=read_scenario_margins(string);
+        
+
+        return scenario_numbers;
+    }
+
+
 }
